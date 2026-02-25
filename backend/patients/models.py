04a4466f9458e82570c2fd4fce6ebbc196929fc5
Sriram <sriram211107@gmail.com>
2026-02-26 01:55:43 +0530

    Fixed backend structure and added full project

---
+++ backend/patients/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Patient(models.Model):
+
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ]
+
+    fullName = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    doctorName = models.CharField(max_length=100)
+    insuranceProvider = models.CharField(max_length=100)
+    policyNumber = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.fullName
