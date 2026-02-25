69ab73e02c4a7c43a3cf764807912df430dad5d0
Sriram <sriram211107@gmail.com>
2026-02-26 03:24:46 +0530

    Fixed allowed hosts properly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://patient-management-1-hwge.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
